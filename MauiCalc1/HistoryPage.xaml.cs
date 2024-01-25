c5dd1d18e2e8090305303c8d5547f7b94720c819
nyox2stupid <patriwachs@gmail.com>
2024-01-25 15:44:39 +0100

    Added Refresh button to HistoryPage.xaml, added functionality to load from database in HistoryPage.xaml.cs

--- MauiCalc1/HistoryPage.xaml.cs
+++ MauiCalc1/HistoryPage.xaml.cs
@@ -1,21 +1,34 @@
+using System.Collections.ObjectModel;
+
 namespace MauiCalc1;
 
 public partial class HistoryPage : ContentPage
 {
-	public HistoryPage()
+    public ObservableCollection<CalcHis> Items { get; set; } = new();
+    CalcHisItemDatabase itemDatabase;
+    public HistoryPage()
 	{
         InitializeComponent();
-	}
+        itemDatabase = new CalcHisItemDatabase();
+    }
 
     private async void OnBackClicked(object sender, EventArgs e)
     {
         await Navigation.PopModalAsync();
     }
-}
-public class Monkey
-{
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public string Details { get; set; }
-    public string ImageUrl { get; set; }
+
+    private async void OnLoadClicked(object sender, EventArgs e)
+    {
+        string ttt = " ";
+        var items = await itemDatabase.GetItemsAsync();
+
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            Items.Clear();
+            //foreach (var item in items)
+            string ttt = items.LastOrDefault().Berechnung;
+        });
+
+        Label label = new Label { Text = ttt };
+    }
 }
